4265955590e9f045e211201de14fe1ba21393b90
Ndumiso Buthelezi <ndzucain@gmail.com>
2025-02-01 12:55:40 +0200

    yt

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Serve from the root on Vercel
+  base: '/', // Update this to match your repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
